488f2c32f04e8f6a52d3140f21db83c324d890ba
low <colder.than.zero@gmail.com>
2023-01-24 19:12:38 +0000

    IT'S ALIVE!!!!!!!!!
    the bot now shows up as online on discord :)

--- src/main.c
+++ src/main.c
@@ -10,6 +10,39 @@ int main()
     struct gateway* g = gateway_init();
     
     if (!gateway_open(g)) {
+        gateway_ping(g);
+
+        struct ws_frame* out1_frame = conn_read(g->c);
+        printf("Received frame:\n\tfin: %hhu\n\topcode: %hhu\n\t"
+               "mask: %hhu\n\tlength: %lu\n\tmask_key: %u\n\tpayload: %s\n",
+               out1_frame->fin, out1_frame->opcode, out1_frame->mask,
+               out1_frame->length, out1_frame->mask_key, out1_frame->payload);
+
+        struct ws_frame in_frame;
+        in_frame.fin = 1;
+        in_frame.opcode = WS_TXT_FRAME;
+        in_frame.mask = 1;
+        in_frame.mask_key = 0;
+        in_frame.length = 178;
+        in_frame.payload = "{\"op\": 2, \"d\":" 
+            "{ \"token\": \"MTA2NjQxNTM3NjM0NDMwMTYxOQ.G89LZy.qM_07NR_iWkaT7iLQrj9xa5qwfwU5YZcf4sZXY\", \"properties\": {"
+                "\"os\": \"linux\","
+                "\"browser\": \"hob\","
+                "\"device\": \"hob\"},"
+            "\"intents\": 0"
+            "}"
+        "}";
+
+        printf("payload: %s\n", in_frame.payload);
+
+        conn_write(g->c, &in_frame);
+
+        out1_frame = conn_read(g->c);
+        printf("Received frame:\n\tfin: %hhu\n\topcode: %hhu\n\t"
+               "mask: %hhu\n\tlength: %lu\n\tmask_key: %u\n\tpayload: %s\n",
+               out1_frame->fin, out1_frame->opcode, out1_frame->mask,
+               out1_frame->length, out1_frame->mask_key, out1_frame->payload);
+
         while (1) {
             printf("Sending ping...\n");
             gateway_ping(g);
@@ -20,7 +53,7 @@ int main()
                    out_frame->fin, out_frame->opcode, out_frame->mask,
                    out_frame->length, out_frame->mask_key, out_frame->payload);
 
-            sleep(5);
+            sleep(g->timeout / 1000);
         }
         
     } else {
